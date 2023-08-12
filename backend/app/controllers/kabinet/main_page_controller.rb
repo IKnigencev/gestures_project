1878bfd8a3ad44f434a4f60ff1ec485917aa6fab
IKnigencev <knigencev2003@mail.ru>
2023-08-12 17:07:44 +0500

    настрйока докера

---
+++ backend/app/controllers/kabinet/main_page_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+##
+# Основная страница кабинета
+class Kabinet::MainPageController < KabinetController
+  ##
+  # Подгрузка данных, главная страница
+  def index
+
+  end
+end
