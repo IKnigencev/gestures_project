7fa98a11107f0db9638cb79c08d3874cdddfb8da
ovechkin <ovechkin_dima03@mail.ru>
2023-08-12 15:31:36 +0500

    login page

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,9 +1,10 @@
 import './app.css'
 import {Route, Routes} from "react-router-dom";
+import {Login} from "../pages/login";
 
 export const App = () => {
 	return <Routes>
-		<Route path={'login'} element={<div>login</div>}/>
+		<Route path={'login'} element={<Login/>}/>
 		<Route path={'registration'} element={<div>registration</div>}/>
 	</Routes>
 }
