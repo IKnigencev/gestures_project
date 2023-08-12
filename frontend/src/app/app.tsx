84c4119c74c8a54b0b3e128cce3d8b4f43d06408
ovechkin <ovechkin_dima03@mail.ru>
2023-08-12 15:07:59 +0500

    routing add

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,5 +1,9 @@
 import './app.css'
+import {Route, Routes} from "react-router-dom";
 
 export const App = () => {
-	return <></>
+	return <Routes>
+		<Route path={'login'} element={<div>login</div>}/>
+		<Route path={'registration'} element={<div>registration</div>}/>
+	</Routes>
 }
