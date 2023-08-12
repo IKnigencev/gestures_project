ead68dcba19e6bcd6324a69c3fe3f8c960ee251d
IKnigencev <knigencev2003@mail.ru>
2023-08-12 22:16:13 +0500

    chan

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -8,21 +8,23 @@ class UsersController < KabinetController
   ##
   # Авторизация пользователя
   def sign_in
-    render json: { email: "Укажите email" }, status: :unprocessable_entity && return if sign_in_params[:email].blank?
-
-    @user = User.find_by(email: sign_in_params[:email])
-    if @user&.authenticate(sign_in_params[:password])
-      token = jwt_encode({ user_id: @user.id })
-      response.set_cookie(
-        :jwt,
-        {
-          value: token,
-          expires: 30.minutes.from_now
-        }
-      )
-      render json: { access_token: token, email: @user.email }, status: :ok
+    if sign_in_params[:email].blank?
+      render json: { email: "Укажите email" }, status: :unprocessable_entity
     else
-      render json: { password: "Неверный пароль" }, status: :unprocessable_entity
+      @user = User.find_by(email: sign_in_params[:email])
+      if @user&.authenticate(sign_in_params[:password])
+        token = jwt_encode({ user_id: @user.id })
+        response.set_cookie(
+          :jwt,
+          {
+            value: token,
+            expires: 30.minutes.from_now
+          }
+        )
+        render json: { access_token: token, email: @user.email }, status: :ok
+      else
+        render json: { password: "Неверный пароль" }, status: :unprocessable_entity
+      end
     end
   rescue Exception => e
     render_errors
